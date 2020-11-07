5121c11e02b2a78a18bb881c1e853395c386d1bd
alanzhang001 <alanzhang001@qq.com>
2020-11-07 19:08:18 +0800

    wepackdpligin demo

--- demos/webpackplugin/plugins/consolePlugin/index.js
+++ demos/webpackplugin/plugins/consolePlugin/index.js
@@ -1,7 +1,8 @@
 /**
  * webpakc 4适用
+ * https://v4.webpack.docschina.org/contribute/writing-a-plugin/#%E5%88%9B%E5%BB%BA%E4%B8%80%E4%B8%AA%E6%8F%92%E4%BB%B6
  */
-
+const child_process = require('child_process');
 // 一个 JavaScript 命名函数。
 function ConsolePlugin(config) {
     this.config = config;
@@ -9,20 +10,18 @@ function ConsolePlugin(config) {
 
 // 在插件函数的 prototype 上定义一个 `apply` 方法。
 ConsolePlugin.prototype.apply = function (compiler) {
-    const that = this;
-    // 指定一个挂载到 webpack 自身的事件钩子。
-    compiler.plugin('ConsolePlugin', function (compilation /* 处理 webpack 内部实例的特定数据。*/, callback) {
-        console.log('This is an example plugin!!!');
-        console.log(JSON.stringify(that.config));
+    // 指定要附加到的事件钩子函数
+    compiler.hooks.emit.tapAsync( 'ConsolePlugin', (compilation, callback) => {
+            // 获取到文件commitid,并写入每个文件中
+            const commitID = child_process.execSync('git rev-parse  HEAD').toString().trim();
+            for(var assert in compilation.assets){
+                const rawSource = compilation.assets[assert];
+                rawSource._value = `/*--${commitID}--*/` + rawSource._value
+            }
 
-        // 现在，设置回调来访问 compilation 中的步骤：
-        compilation.plugin("optimize", function() {
-            console.log("Assets are being optimized.");
-        });
-
-        // 功能完成后调用 webpack 提供的回调。
-        callback();
-    });
+            callback();
+        }
+    );
 };
 
 module.exports = ConsolePlugin;
